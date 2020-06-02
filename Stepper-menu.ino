e64b27341e90b63e8c41e744d0304cd719c70ece
Cimanes <63605962+Cimanes@users.noreply.github.com>
2020-06-02 17:44:36 +0200

    Update Stepper-menu.ino

--- Stepper-menu.ino
+++ Stepper-menu.ino
@@ -3,8 +3,8 @@
  * This sketch can be used to move a stepper motor. 
  * It was developed and tested using the Stepper motor "28BYJ-48" and driver module "ULN2003"
  * A power supply (5 VDC) is used to feed the motor.
- * The rest of speed settings (1 to 8) will be calculated proportionally
  * The constant "Rpm_Max" will set the Max speed for the potentiometer. 
+ * The rest of speed settings (1 to 8) will be calculated proportionally
  *   
  * There is an interactive menu in the Serial Monitor. 
  * The user can select three modes of operation "A, B, C" (incrasing torque and decreasing consumption)
@@ -19,9 +19,9 @@
  * 
  * The interactive menu looks as follows: 
  * 
- * Select direction: 'F' (fwd)  /  'R' (Rear)
- * Select torque   : 'A' (low)  /  'B' (mid) / 'C' (high)
- * Select speed    : '0' (min) ... '9' (max) / 'T' (Turbo) / 'P' (Pot control)";
+ * Select direction: 'F' (fwd)    /  'R' (rear)
+ * Select torque   : 'A' (low)    /  'B' (mid) / 'C' (high)
+ * Select speed    : Rpm (value)  /  'P' (potentiometer)
  * --------------------------------------------------------
  * Direction = F / Torque = B / Rpm = 0.00 / Dt = 7000 / Deg = 0.00 / Status = Stop
  * 'S' to start / 'X' to stop / 'M' to refresh menu / 'T' to go to a Target angle
@@ -63,11 +63,11 @@ const byte IN[4] =     { 8,   9,  10,  11 } ;     // Define pins connected to th
 const byte potAI = 0                        ;     // Define pin for analog input from potentiometer (speed control)
 
 // Define motor specs:
-const int Steps   = 4096    ;     // Number of steps for "8 step sequence". 
-const float Rpm_Max  =  20  ;     // Max Rpm (recommended 20 for 28BYJ-48 stepper motor)
+const int Steps     = 4096  ;     // Number of steps for "8 step sequence". 
+const float Rpm_Max  =  18  ;     // Max Rpm (recommended 18 for 28BYJ-48 stepper motor)
 
 // Define initial conditions
-bool Pot          = LOW   ;       // This variable will be HIGH when using potentiometer to control the speed.
+bool Pot          = false ;       // This variable will be HIGH when using potentiometer to control the speed.
 String Input      = "X"   ;       // Character array received from Serial Monitor
 char Initial      = 'X'   ;       // Initial character of the String "Input"
 char Direction    = 'F'   ;       // Turn direction (Forward / Rear); Forward by default
@@ -90,9 +90,9 @@ void setup()
 // Display initial menu (user selectable options) and initial conditions:
   Serial.begin(9600);
   Serial.println("--------------------------------------------------------"); 
-  Serial.println("Select direction: 'F' (fwd)  /  'R' (Rear)");
-  Serial.println("Select torque   : 'A' (low)  /  'B' (mid) / 'C' (high)");
-  Serial.println("Select speed    : Rpm value  /  'P' (Potentiometer)");
+  Serial.println("Select direction: 'F' (fwd)    /  'R' (rear)");
+  Serial.println("Select torque   : 'A' (low)    /  'B' (mid) / 'C' (high)");
+  Serial.println("Select speed    : Rpm (value)  /  'P' (potentiometer)");
   Serial.println("--------------------------------------------------------"); 
   Serial.println("Direction = " + String(Direction) + " / Torque = " + String(Torque) + 
                  " / Rpm = " + String(Rpm) + " / Dt = " + String(Dt) + 
@@ -105,7 +105,7 @@ void loop() {
   if(Serial.available()) 
   {
     Input = Serial.readStringUntil('\n');               // Read incoming string from Serial Monitor
-    if (Input == "T") goTarget();                         // Selection "go to target angle"
+    if (Input == "T") goTarget();                       // Selection "go to target angle"
     else menu();                                        // Selection of menu to define running conditions
     Dt = KDt / Rpm;                                     // Calculate time delay
 
@@ -118,7 +118,10 @@ void loop() {
     Serial.println();
   }
 
-  if (Status == "Stop" or Rpm == 0) return;             // Stop if required or if selected Rpm is "0"
+  if (Status == "Stop" or Rpm == 0) {                   // Stop if requested or if selected Rpm is "0"
+    for (int i = 0; i < 4; i++) digitalWrite(IN[i], LOW);
+    return;
+  }
   if (Torque == 'A') { Sequence = 4;  Dt *= 2; A(); }   // Low torque selection (4 steps, Double delay)
   if (Torque == 'B') { Sequence = 8;           B(); }   // Mid torque selection (8 steps)
   if (Torque == 'C') { Sequence = 4;  Dt *= 2; C(); }   // Low torque selection (4 steps, Double delay)
@@ -129,9 +132,9 @@ void menu() {
   Initial = Input.charAt(0);
   if (Input == "M") {
     Serial.println("--------------------------------------------------------"); 
-    Serial.println("Select direction: 'F' (fwd)    /  'R' (Rear)");
+    Serial.println("Select direction: 'F' (fwd)    /  'R' (rear)");
     Serial.println("Select torque   : 'A' (low)    /  'B' (mid) / 'C' (high)");
-    Serial.println("Select speed    : Rpm (value)  /  'P' (Potentiometer)");
+    Serial.println("Select speed    : Rpm (value)  /  'P' (potentiometer)");
     Serial.println("--------------------------------------------------------"); 
   }
   if (Input == "X")                                 Status    = "Stop";
@@ -139,50 +142,66 @@ void menu() {
   if (Input == "A" or Input == "B" or Input == "C") Torque    = Input.charAt(0);    // Torque selection
   if (Input == "F" or Input == "R")                 Direction = Input.charAt(0);    // Direction selection
   if (Input == "P") {                                                               // Pot control for speed
-    Pot = HIGH;  
+    Pot = true;  
     Rpm = analogRead(potAI) * Rpm_Max / 1093; 
   }
   if (isDigit(Initial)) { 
-    Pot = LOW;
+    Pot = false;
     Rpm = Input.toFloat();
-    if (Rpm > Rpm_Max) Serial.println("Speed too high");
+    if (Rpm > Rpm_Max) {
+      Serial.println("Speed too high");
+      Serial.println();
+    }
   }
 }
 
 void goTarget() {
 start:
-// The following four lines (147 to 10) are optional, to use moderate speed and mid torque chasing target.
+// The following four lines (162 to 164) are optional, to use moderate speed and mid torque chasing target.
 // They can be bypassed to use the pre-existing speed and torque selection:
 // ----------------
-  Sequence = 8;                                     // Select mid Torque
-  Rpm      = Rpm_Max / 2;                           // Normal speed (50% Rpm)
-  Dt       = KDt / Rpm          ;                   // Calculate time delay for 10 Rpm.
+  Sequence = 8;                                         // Select mid Torque
+  Rpm      = Rpm_Max / 2;                               // Normal speed (50% Rpm)
+  Dt       = KDt / Rpm          ;                       // Calculate time delay for 10 Rpm.
 // ----------------
+  for (int i = 0; i < 4; i++) digitalWrite(IN[i], LOW); // De-energize all coils
+
   Serial.println("Target mode; Deg = " + String(Deg));    
   Serial.println("Enter 'X' to exit Target mode");
-  Serial.print("Enter target (0 to 360 deg): ");    // Prompt for angle target
-  while (!Serial.available());                      // Wait to receive the serial input
-  Input  = Serial.readStringUntil('\n');            // Read the angle target
-  if (Input == "X") {                               // Exit Target mode when user enters "X"
-    Serial.println("Exit"); 
+  Serial.print("Enter target (0 to 360 deg): ");        // Prompt for target angle
+  while (!Serial.available());                          // Wait to receive the serial input
+    Input  = Serial.readStringUntil('\n');                // Read the angle target
+  
+  if (Input == "X") {                                   // Exit Target mode when user enters "X"
+    Status = "Stop";
+    Serial.println("Exit Target mode");
+    Serial.println(); 
     return;
   }
-  Target = Input.toInt();                           // Convert the received "string" to integer
-  Diff   = abs(Target - Deg);                       // Calculate difference
-  Serial.println(Target);                           // Print the selected target 
-  Serial.println("--------------------------");
+  Target = Input.toInt();                               // Convert the received "string" to integer
+  if (Target == 0 and Deg > 180) Target = 360;          // Fix issues with transition "360 to 0"
+  if (Target > 360 or Target < 0) {                     // Reject target if it is > 360º
+    Serial.println("Target out of range");
+    Serial.println();
+    goto start;
+  }
   
-  if (Diff < Deadband) return;                      // Exit if the angle is already close enough to the Target
+  Diff   = abs(Target - Deg);                           // Calculate difference
+  Serial.println(Target);                               // Print the selected target 
+  Serial.println();
+  
+  if (Diff < Deadband) return;                          // Exit if the angle is already close enough to the Target
+  
   if ( (Diff < 180 and Deg < Target) or (Diff > 180 and Deg > Target) ) {
     do {
       for (byte i = 0; i < Sequence; i++) {
         for (byte j = 0; j < 4; j++)  digitalWrite(IN[j], seqB[i][j]); 
         delayMicroseconds(Dt);
       }
-      Deg = Deg + 2880 / float(Steps);                    // Increase angle (8 * 360 / Steps = 2880 / Steps)
+      Deg = Deg + 2880 / float(Steps);                        // Increase angle (8 * 360 / Steps = 2880 / Steps)
       if (Deg > 360)     Deg = Deg - 360;
-      if (Diff < 1 and Diff < abs(Target - Deg)) return;  // Stop if difference increases (if deadband was set too tight)
-      Diff = abs(Target - Deg);                           // Calculate new difference
+      if (Diff < 1 and Diff < abs(Target - Deg)) goto start;  // Stop if difference increases (if deadband was set too tight)
+      Diff = abs(Target - Deg);                               // Calculate new difference
     }
     while (Diff > Deadband and !Serial.available());      // Stop when difference is lower than deadband or user enters a command
     if (Serial.available()) Serial.println("Halted by user");
@@ -197,7 +216,7 @@ start:
       }
       Deg = Deg - 2880 / float(Steps);
       if (Deg < 0)     Deg = Deg + 360;
-      if (Diff < 1 and Diff < abs(Target - Deg)) return;  // Stop if difference increases (if deadband was set too tight)
+      if (Diff < 1 and Diff < abs(Target - Deg)) goto start;  // Stop if difference increases (if deadband was set too tight)
       Diff = abs(Target - Deg);                           // Calculate new difference
     }
     while (Diff > Deadband and !Serial.available());      // Stop when difference is lower than deadband or user enters a command
@@ -207,7 +226,6 @@ start:
 }
 
 void A() {
-
   if (Direction == 'F')
     do {
       if (Pot == HIGH)  Dt = KDt / Rpm_Max * 1023 / analogRead(potAI);
